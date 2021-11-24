ff8e793f6560fd33626dee080bd14ecc4a0ff6ea
Davef <76170782+davefdev@users.noreply.github.com>
2021-11-24 21:10:25 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import SearchBar from './components/search-bar';
 
+//functional component
+//returns a template, doesnt have too much config, or methods, or functions inside a class that is used to manipulate/create logic
+//just returns a template )what is being rendered in the UI
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <SearchBar />
       </header>
     </div>
   );
---
+++ src/components/search-bar.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+//this is a class component
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: "",
+    };
+  }
+  onInputChange(e) {
+      this.setState({
+          location: e.target.value
+      });
+  }
+
+  onFormSubmit(e) {
+    e.preventDefault();
+
+
+  }
+  //render function is triggered whenever updating the state using the setState function
+  render() {
+    const location = this.state.location;
+
+    return (
+      <form onSubmit={(e) => this.onFormSubmit(e)}>
+    
+        <button type="submit">Search</button>
+        <input
+          id="id"
+          name="search"
+          value={location}
+          onChange={(e) => this.onInputChange(e)}
+        ></input>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
