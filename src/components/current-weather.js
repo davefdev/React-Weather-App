f42d8554077740d7364eab9532c3ea48940c302d
Davef <76170782+davefdev@users.noreply.github.com>
2021-12-03 10:17:46 +0000

    KKY

--- src/components/current-weather.js
+++ src/components/current-weather.js
@@ -5,12 +5,18 @@ class CurrentWeather extends React.Component {
     return (
       <div className="current-weather">
         <div className="current-weather__content">
-          <p className="current-weather__temp">{this.props.currentTemperature}</p>
-          <p className="current-weather__description">{this.props.description}</p>
+          <p className="current-weather__temp">
+            {this.props.currentTemperature}
+          </p>
+          <p className="current-weather__description">
+            {this.props.description}
+          </p>
           <img className="current-weather__icon" url="" />
         </div>
         <div>
-          <p className="current-weather__feels-like">Feels Like{this.props.feelsLike}</p>
+          <p className="current-weather__feels-like">
+            Feels Like{this.props.feelsLike}
+          </p>
         </div>
       </div>
     );
