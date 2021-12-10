e5da7951872bd3bd35a40c34d3e80cfe07af1547
Davef <76170782+davefdev@users.noreply.github.com>
2021-12-10 19:48:35 +0000

    yh

--- src/components/forecast-weather.js
+++ src/components/forecast-weather.js
@@ -2,7 +2,10 @@ import React from "react";
 
 class Forecast extends React.Component {
   render() {
-    this.props.forecast.map({f} => {
+    const forecastItem = this.props.forecast.map({f, i} => {
+      const key = `forecast-item_${i}>`
+      const url = `http://openweathermap.org/img/wn/${f.weather[0].icon}@2x.png`;
+      <div className="forecast-item" key=`forecast-item_${i}>
       let ampm = 'AM';
       let hour = new Date(f.dt * 1000).getHours();
       
@@ -14,17 +17,21 @@ class Forecast extends React.Component {
 
       }
       return (
-      <div className="forecast-item">
-        <p className="forecast-item__hour">  {hour}:00 {ampm}    </p>
-        <p>{f.temp}</p>
-        <p>{f.weather[0].main}</p>
+      <div className="forecast-item" key={key}>
+        <p className="forecast-item__hour">  {hour}:00 {ampm} {""}   </p>
+        <p className="forecast-item__temp">{f.temp}</p>
+        <img src={url} alt={f.weather[0].description}/>
+        <p className="forecast-item__description">{f.weather[0].main}</p>
        </div>
-
-
       );
     });
 
-    return <div className="forecast">
+    return (
+    <div className="forecast">
+        {forecastItem}
+
+    </div>
+    );
       
 
      
