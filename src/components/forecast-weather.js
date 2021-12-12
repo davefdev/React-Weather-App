c767d43170ce0ba504d06ae313735dce54130d10
Davef <76170782+davefdev@users.noreply.github.com>
2021-12-12 12:26:56 +0000

    GBTG

--- src/components/forecast-weather.js
+++ src/components/forecast-weather.js
@@ -3,35 +3,28 @@ import React from "react";
 class Forecast extends React.Component {
   render() {
     const forecastItem = this.props.forecast.map({f, i} => {
-      const key = `forecast-item_${i}>`
+      const key = `forecast-item_${i}`
       const url = `http://openweathermap.org/img/wn/${f.weather[0].icon}@2x.png`;
-      <div className="forecast-item" key=`forecast-item_${i}>
-      let ampm = 'AM';
+      let ampm = "AM";
       let hour = new Date(f.dt * 1000).getHours();
       
       
       if (hour > 12) {
-        
         hour = hour - 12;
-        ampm = 'PM';
-
+        ampm = "PM";
       }
       return (
-      <div className="forecast-item" key={key}>
-        <p className="forecast-item__hour">  {hour}:00 {ampm} {""}   </p>
-        <p className="forecast-item__temp">{f.temp}</p>
-        <img src={url} alt={f.weather[0].description}/>
-        <p className="forecast-item__description">{f.weather[0].main}</p>
-       </div>
+        <div className="forecast-item" key={key}>
+          <p className="forecast-item__hour">  {hour}:00 {ampm} {""}   </p>
+          <p className="forecast-item__temp">{f.temp}</p>
+          <img src={url} alt={f.weather[0].description}/>
+          <p className="forecast-item__description">{f.weather[0].main}</p>
+        </div>
       );
     });
 
-    return (
-    <div className="forecast">
-        {forecastItem}
-
-    </div>
-    );
+    return  <div className="forecast">{forecastItem}</div>
+    
       
 
      
